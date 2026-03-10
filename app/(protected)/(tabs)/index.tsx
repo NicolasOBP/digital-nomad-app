d049b8a4a0bd53e4f2084d6d4043371e6361b8cf
NicolasOBP <nicolasobp15@gmail.com>
2026-03-10 10:56:33 -0300

    FlatList and adjustments

--- app/(protected)/(tabs)/index.tsx
+++ app/(protected)/(tabs)/index.tsx
@@ -1,17 +1,32 @@
 import { CityCard } from "@/src/components/CityCard";
 import { cityPreviewList } from "@/src/data/cities";
 import { Screen } from "@/src/template/Screen";
+import { useAppTheme } from "@/src/theme/useAppTheme";
 import { CityPreview } from "@/src/types";
+import { useScrollToTop } from "@react-navigation/native";
+import { useRef } from "react";
 import { FlatList, ListRenderItemInfo } from "react-native";
 
 export default function HomeScreen() {
+  const { spacing } = useAppTheme();
+  const flatListRef = useRef(null);
+
+  useScrollToTop(flatListRef);
+
   function renderItem({ item }: ListRenderItemInfo<CityPreview>) {
     return <CityCard cityPreview={item} />;
   }
 
   return (
     <Screen>
-      <FlatList data={cityPreviewList} renderItem={renderItem} />
+      <FlatList
+        ref={flatListRef}
+        contentContainerStyle={{ gap: spacing.padding }}
+        data={cityPreviewList}
+        renderItem={renderItem}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+      />
     </Screen>
   );
 }
