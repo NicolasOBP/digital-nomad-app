1477737e7ec4f159e764aed7ea885d0850b51fc8
NicolasOBP <nicolasobp15@gmail.com>
2026-03-20 11:19:24 -0300

    Maps with react-native-maps

--- src/containers/CityDetailsMap.tsx
+++ src/containers/CityDetailsMap.tsx
@@ -1,10 +1,31 @@
+import MapView from "react-native-maps";
+
 import { Box } from "../components/Box";
 import { Text } from "../components/Text";
+import { City } from "../types";
 
-export function CityDetailsMap() {
+type Props = Pick<City, "location">;
+
+export function CityDetailsMap({ location }: Props) {
   return (
     <Box p="padding">
-      <Text>City Details Map</Text>
+      <Text variant="title22" mb="s16">
+        Mapa
+      </Text>
+      <Box borderRadius="default" overflow="hidden">
+        <MapView
+          style={{
+            width: "100%",
+            height: 200,
+          }}
+          initialRegion={{
+            latitude: location.latitude,
+            longitude: location.longitude,
+            latitudeDelta: 0.0922,
+            longitudeDelta: 0.0421,
+          }}
+        />
+      </Box>
     </Box>
   );
 }
