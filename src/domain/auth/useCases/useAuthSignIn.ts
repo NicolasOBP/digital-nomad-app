fe51fdbf71d79b1244ca81555ef72ec767180fa6
NicolasOBP <nicolasobp15@gmail.com>
2026-05-03 11:11:56 -0300

    useMutation

--- src/domain/auth/useCases/useAuthSignIn.ts
+++ src/domain/auth/useCases/useAuthSignIn.ts
@@ -1,6 +1,7 @@
+import { useMutation } from "@tanstack/react-query";
+
 import { useFeedbackService } from "@/src/infra/feedbackService/FeedbackProvider";
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
-import { useAppMutation } from "@/src/infra/useCases/useAppMutation";
 
 import { useAuth } from "../AuthContext";
 import { AuthUser } from "../AuthUser";
@@ -10,8 +11,12 @@ export function useAuthSignIn() {
   const { send } = useFeedbackService();
   const { saveAuthUser } = useAuth();
 
-  return useAppMutation<AuthUser, { email: string; password: string }>({
-    mutateFn: ({ email, password }) => auth.signIn(email, password),
+  const { mutate, error, isPending } = useMutation<
+    AuthUser,
+    unknown,
+    { email: string; password: string }
+  >({
+    mutationFn: ({ email, password }) => auth.signIn(email, password),
     onSuccess: (authUser) => {
       saveAuthUser(authUser);
       send({
@@ -27,4 +32,24 @@ export function useAuthSignIn() {
       });
     },
   });
+
+  return { mutate, error, isPending };
+
+  // return useAppMutation<AuthUser, { email: string; password: string }>({
+  //   mutateFn: ({ email, password }) => auth.signIn(email, password),
+  //   onSuccess: (authUser) => {
+  //     saveAuthUser(authUser);
+  //     send({
+  //       type: "success",
+  //       message: `signed in as ${authUser.email}`,
+  //     });
+  //   },
+  //   onError: (error) => {
+  //     send({
+  //       type: "error",
+  //       message: `erro ao fazer login`,
+  //       description: error.message,
+  //     });
+  //   },
+  // });
 }
