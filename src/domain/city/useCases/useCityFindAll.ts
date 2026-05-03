0fe40d6526f4e30c246b9aff259cabbf84656fbb
NicolasOBP <nicolasobp15@gmail.com>
2026-05-03 11:00:34 -0300

    useQuery

--- src/domain/city/useCases/useCityFindAll.ts
+++ src/domain/city/useCases/useCityFindAll.ts
@@ -1,13 +1,26 @@
+import { useQuery } from "@tanstack/react-query";
+
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
-import { useAppQuery } from "@/src/infra/useCases/useAppQuery";
 
 import { CityFindAllFilter } from "../ICityRepo";
 
 export function useCityFindAll(filters: CityFindAllFilter) {
   const { city } = useRepository();
 
-  return useAppQuery(
-    () => city.findAll(filters),
-    [filters.name, filters.categoryId],
-  );
+  const { data, error, isLoading, isFetching } = useQuery({
+    queryKey: ["city", filters.name, filters.categoryId],
+    queryFn: () => city.findAll(filters),
+  });
+
+  return {
+    data,
+    error,
+    isLoading,
+    isFetching,
+  };
+
+  // return useAppQuery(
+  //   () => city.findAll(filters),
+  //   [filters.name, filters.categoryId],
+  // );
 }
