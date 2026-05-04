3865e1d264809cc28d0a7bec03c726370c97cffd
NicolasOBP <nicolasobp15@gmail.com>
2026-05-04 10:50:07 -0300

    clear cache on sign out

--- src/domain/auth/useCases/useAuthSignOut.ts
+++ src/domain/auth/useCases/useAuthSignOut.ts
@@ -1,3 +1,5 @@
+import { useQueryClient } from "@tanstack/react-query";
+
 import { useRepository } from "@/src/infra/repositories/RepositoryProvider";
 import { useAppMutation } from "@/src/infra/useCases/useAppMutation";
 
@@ -6,9 +8,12 @@ import { useAuth } from "../AuthContext";
 export function useAuthSignOut() {
   const { removeAuthUser } = useAuth();
   const { auth } = useRepository();
+  const queryClient = useQueryClient();
+
   return useAppMutation({
     mutationFn: () => auth.signOut(),
     onSuccess: () => {
+      queryClient.clear();
       removeAuthUser();
     },
   });
