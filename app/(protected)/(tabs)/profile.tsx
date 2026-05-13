c29ed940d1b0f2e6583dc4e63310f7df960a4069
NicolasOBP <nicolasobp15@gmail.com>
2026-05-13 09:39:27 -0300

    FavoriteCityCard

--- app/(protected)/(tabs)/profile.tsx
+++ app/(protected)/(tabs)/profile.tsx
@@ -6,7 +6,7 @@ import { useAuthGetUser } from "@/src/domain/auth/useCases/useAuthGetUser";
 import { useAuthSignOut } from "@/src/domain/auth/useCases/useAuthSignOut";
 import { useCityFindAllFavorites } from "@/src/domain/city/useCases/useCityFindAllFavorites";
 import { Box } from "@/src/ui/components/Box";
-import { CityCard } from "@/src/ui/components/CityCard";
+import { FavoriteCityCard } from "@/src/ui/components/FavoriteCityCard";
 import { Icon } from "@/src/ui/components/Icon";
 import { Text } from "@/src/ui/components/Text";
 import { ProfileHeader } from "@/src/ui/containers/Profile/ProfileHeader";
@@ -22,10 +22,11 @@ export default function ProfileScreen() {
       <SafeAreaView>
         {authUser && <ProfileHeader authUser={authUser} />}
 
-        {favoriteList?.map((cityPreview) => (
-          <CityCard cityPreview={cityPreview} key={cityPreview.id} />
-        ))}
-
+        <Box mt="s16" rowGap="s16">
+          {favoriteList?.map((cityPreview) => (
+            <FavoriteCityCard cityPreview={cityPreview} key={cityPreview.id} />
+          ))}
+        </Box>
         <Pressable onPress={signOut}>
           <Box
             mt="s24"
---
+++ src/ui/components/FavoriteCityCard.tsx
@@ -0,0 +1,71 @@
+import { Link } from "expo-router";
+import { Image, Pressable, useWindowDimensions } from "react-native";
+
+import { CityPreview } from "@/src/domain/city/City";
+import { useCityToggleFavorite } from "@/src/domain/city/useCases/useCityToggleFavorite";
+
+import { useAppTheme } from "../theme/useAppTheme";
+
+import { Box, TouchableOpacityBox } from "./Box";
+import { Icon } from "./Icon";
+import { Text } from "./Text";
+
+type FavoriteCityCardProps = {
+  cityPreview: CityPreview;
+};
+
+export function FavoriteCityCard({ cityPreview }: FavoriteCityCardProps) {
+  const { borderRadii } = useAppTheme();
+  const { mutate: toggleFavorite } = useCityToggleFavorite();
+
+  const { width } = useWindowDimensions();
+  const IMAGE_WIDTH = width * 0.3;
+  const IMAGE_HIGHT = IMAGE_WIDTH * 0.75;
+
+  return (
+    <Link push href={`/city-details/${cityPreview.id}`} asChild>
+      <Pressable>
+        <Box
+          flexDirection="row"
+          bg="gray1"
+          p="s12"
+          borderRadius="default"
+          justifyContent="space-between"
+        >
+          <Box flexDirection="row">
+            <Image
+              source={
+                typeof cityPreview.coverImage === "number"
+                  ? cityPreview.coverImage
+                  : { uri: cityPreview.coverImage }
+              }
+              style={[
+                {
+                  width: IMAGE_WIDTH,
+                  height: IMAGE_HIGHT,
+                  borderRadius: borderRadii.default,
+                },
+              ]}
+            />
+
+            <Box ml="s16" justifyContent="center">
+              <Text variant="title16">{cityPreview.name}</Text>
+              <Text variant="text16">{cityPreview.country}</Text>
+            </Box>
+          </Box>
+
+          <Box>
+            <TouchableOpacityBox
+              alignSelf="flex-end"
+              onPress={() => {
+                toggleFavorite({ cityId: cityPreview.id, isFavorite: false });
+              }}
+            >
+              <Icon name="Favorite-outline" color="text" />
+            </TouchableOpacityBox>
+          </Box>
+        </Box>
+      </Pressable>
+    </Link>
+  );
+}
