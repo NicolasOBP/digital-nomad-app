627e4b07e80689addca65304ba2820ca56088038
NicolasOBP <nicolasobp15@gmail.com>
2026-05-16 11:32:21 -0300

    city detail - loading, error and animation

--- app/(protected)/city-details/[id].tsx
+++ app/(protected)/city-details/[id].tsx
@@ -1,7 +1,7 @@
 import { useLocalSearchParams } from "expo-router";
-import { ActivityIndicator, Pressable } from "react-native";
+import { Pressable } from "react-native";
 
-import { useSharedValue } from "react-native-reanimated";
+import Animated, { FadeIn, useSharedValue } from "react-native-reanimated";
 
 import { useCityFindById } from "@/src/domain/city/useCases/useCityFindById";
 import { Divider } from "@/src/ui/components/Divider";
@@ -14,6 +14,8 @@ import { CityDetailsRelatedCities } from "@/src/ui/containers/CityDetailsRelated
 import { CityDetailsTouristAttraction } from "@/src/ui/containers/CityDetailsTouristAttraction";
 import { Screen } from "@/src/ui/template/Screen";
 
+const PAGE_ANIMATION_TIME = 1000;
+
 export default function CityDetailsScreen() {
   const { id } = useLocalSearchParams<{ id: string }>();
   const { data: city, error, isLoading } = useCityFindById(id);
@@ -24,7 +26,7 @@ export default function CityDetailsScreen() {
     bottomSheetIsOpen.value = !bottomSheetIsOpen.value;
   }
 
-  if (error) {
+  if (isLoading) {
     return (
       <Screen
         padding="padding"
@@ -32,53 +34,66 @@ export default function CityDetailsScreen() {
         alignItems="center"
         flex={1}
       >
-        <Text>City not found</Text>
+        <Text>Carregando dados...</Text>
       </Screen>
     );
   }
 
-  if (isLoading || !city) {
-    return <ActivityIndicator size={50} />;
+  if (!city || error) {
+    return (
+      <Screen
+        padding="padding"
+        justifyContent="center"
+        alignItems="center"
+        flex={1}
+      >
+        <Text>Erro ao carregar cidades</Text>
+      </Screen>
+    );
   }
 
   return (
     <>
       <Screen style={{ paddingHorizontal: 0 }} scrollable>
-        <CityDetailsHeader
-          id={city.id}
-          categories={city.categories}
-          coverImage={city.coverImage}
-          isFavorite={city.isFavorite}
-        />
+        <Animated.View entering={FadeIn.duration(PAGE_ANIMATION_TIME)}>
+          <CityDetailsHeader
+            id={city.id}
+            categories={city.categories}
+            coverImage={city.coverImage}
+            isFavorite={city.isFavorite}
+          />
 
-        <CityDetailsInfo
-          name={city.name}
-          country={city.country}
-          description={city.description}
-        />
+          <CityDetailsInfo
+            name={city.name}
+            country={city.country}
+            description={city.description}
+          />
 
-        <Divider paddingHorizontal="padding" />
+          <Divider paddingHorizontal="padding" />
 
-        <CityDetailsTouristAttraction
-          touristAttractions={city.touristAttractions}
-        />
+          <CityDetailsTouristAttraction
+            touristAttractions={city.touristAttractions}
+          />
 
-        <Divider paddingHorizontal="padding" />
+          <Divider paddingHorizontal="padding" />
 
-        <Pressable onPress={toggleBottomSheet}>
-          <CityDetailsMap location={city.location} />
-        </Pressable>
+          <Pressable onPress={toggleBottomSheet}>
+            <CityDetailsMap location={city.location} />
+          </Pressable>
 
-        <Divider paddingHorizontal="padding" />
+          <Divider paddingHorizontal="padding" />
 
-        <CityDetailsRelatedCities id={city.id} />
+          <CityDetailsRelatedCities id={city.id} />
+        </Animated.View>
       </Screen>
 
-      <BottomSheetMap
-        isOpen={bottomSheetIsOpen}
-        location={city.location}
-        onPress={toggleBottomSheet}
-      />
+      <Animated.View entering={FadeIn.duration(0).delay(PAGE_ANIMATION_TIME)}>
+        <BottomSheetMap
+          isOpen={bottomSheetIsOpen}
+          location={city.location}
+          onPress={toggleBottomSheet}
+        />
+      </Animated.View>
     </>
   );
 }
