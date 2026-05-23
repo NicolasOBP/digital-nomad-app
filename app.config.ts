e46eed883d5cc2eb8df26f67206e7aae02c4f02c
NicolasOBP <nicolasobp15@gmail.com>
2026-05-23 17:17:46 -0300

    Android Maps - API Key

--- app.config.ts
+++ app.config.ts
@@ -22,6 +22,11 @@ module.exports = {
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
     package: "com.nicolasobp.digitalnomadapp",
+    config: {
+      googleMaps: {
+        apiKey: process.env.GOOGLE_MAPS_API_KEY,
+      },
+    },
   },
   web: {
     output: "static",
